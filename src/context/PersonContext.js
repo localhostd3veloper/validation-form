ff06fcea7cc17167ade27c19cb8ae02df2dbc8cd
localhostd3veloper <gautamanand022@gmail.com>
2023-01-25 19:54:28 +0530

    code setup with react router dom

---
+++ src/context/PersonContext.js
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "../components/HomePage";
+import Forms from "../components/Forms";
+
+export const PersonContext = createContext();
+
+export function usePerson() {
+  return useContext(PersonContext);
+}
+
+export const TicketProvider = () => {
+  useEffect(() => {
+    async function fetchData() {}
+    fetchData();
+  }, []);
+
+  useEffect(() => {}, []);
+
+  const value = {};
+
+  return (
+    <PersonContext.Provider value={value}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/checkout" element={<Forms />} />
+        </Routes>
+      </BrowserRouter>
+    </PersonContext.Provider>
+  );
+};
